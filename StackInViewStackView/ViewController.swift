e3126c7deea1b4d624e742a7a11c73432d30e37f
kokotata421 <kokotata421@gmail.com>
2018-10-29 03:05:38 +0900

    first commit

---
+++ StackInViewStackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackInViewStackView
+//
+//  Created by Kota Kawanishi on 2018/10/28.
+//  Copyright © 2018年 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
